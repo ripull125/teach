d383e82ca4a4a221f988bc446fe538b30b4fa80c
dean1210e <deanelbayar@gmail.com>
2023-01-04 10:42:30 -0500

    get bum function

--- dean.py
+++ dean.py
@@ -1,1 +1,8 @@
+def get_nums():
+    x = input("How many numbers do you want to input?")
+    list = []
+    for i in range(x):
+        more = input("Add x more numbers")
+        list.append(more)
+    return list
 
